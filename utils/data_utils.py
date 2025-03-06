d963e0581c49e30efa99a619b81ddf834789689c
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-06 13:09:36 +0900

    modularize code for baseline

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,6 +1,6 @@
 import pysrt
-import sys
-
+import os
+from sklearn.metrics import confusion_matrix
 def srt_time_to_seconds(srt_time):
     # Split the time string by colon and comma
     try:
@@ -35,3 +35,38 @@ def read_srt(input_file_path):
         subs.append(sub)
     return subs
 
+def write_logs(out_folder, predictions):
+    out_file = os.path.join(out_folder, "logs.txt")
+    print(f"Generation stored at {out_file}")
+    with open(out_file, 'a') as the_file:
+        for t, ut in enumerate(predictions):
+            the_file.write(f"{t}: {ut}\n")
+    return out_file
+def get_commentary_path(commentary_directory, game_path):
+    game_path = os.path.basename(game_path)
+    commentary_path = [os.path.join(commentary_directory, file) for file in os.listdir(commentary_directory) if
+     file.endswith('.srt') and os.path.isfile(os.path.join(commentary_directory, file)) and "kyakkan" in file
+         and game_path in file]
+    if len(commentary_path) > 0:
+        commentary_path = commentary_path[0]
+    else:
+        commentary_path = None
+    return commentary_path
+
+
+def remove_repeatitions(utterences):
+    pred_utterences_cleaned = []
+    previous = ""
+    for pred_utterence in utterences:
+        if previous != pred_utterence:
+            pred_utterences_cleaned.append(pred_utterence)
+        else:
+            pred_utterences_cleaned.append("")
+        previous = pred_utterence
+    return pred_utterences_cleaned
+
+
+def compute_metrics(ref_timing, pred_timing):
+    correlations = [1 if a == b else 0 for a, b in zip(ref_timing, pred_timing)]
+    cm = confusion_matrix(ref_timing, pred_timing)
+    return {"confusion_matrix": cm, "correlation":correlations.count(1)}
---
+++ utils/video_utils.py
@@ -0,0 +1,51 @@
+import cv2
+import torch
+import numpy as np
+from PIL import Image
+def get_video_info(path):
+    video = cv2.VideoCapture(path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    duration = int(total_frames/fps)
+    return {"total_frames": total_frames, "frames_per_second": fps,
+            "duration":duration }
+def sample_frames(path, num_frames, start_frame = None, end_frame = None, format = "images"):
+
+    video = cv2.VideoCapture(path)
+    if start_frame is not None:
+        video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+    if end_frame is None:
+        end_frame = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    if start_frame - end_frame < num_frames:
+        print ()
+    interval = (end_frame - start_frame) // num_frames
+    frames = []
+    take_next_frame = False
+
+
+    for i in range(end_frame - start_frame):
+        try:
+            ret, frame = video.read()
+            pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            if not ret:
+                continue
+            if i % interval == 0:
+                frames.append(pil_img)
+            if take_next_frame:
+                frames.append(pil_img)
+                take_next_frame = False
+        except Exception as e:
+            print (f"Failed to get frame number {i} for video: {path} due to Exception {e} \n Using the next frame instead.")
+            if i % interval == 0:
+                take_next_frame = True
+
+    video.release()
+    if format == "video":
+        frames = [np.array(frame) for frame in frames]
+        frames = np.stack(frames, axis=0)
+    return frames
+
+def replace_video_with_images(text, frames):
+  return text.replace("<video>", "<image>" * frames)
