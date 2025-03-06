b1a2221c460e30853c4ae09253dc08993cbe0050
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-06 13:01:58 +0900

    modularize code for baseline

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from transformers import LlavaProcessor, LlavaForConditionalGeneration
-import os
-import cv2
-import av
-import torch
-import numpy as np
-from huggingface_hub import hf_hub_download
 from transformers import LlavaNextVideoProcessor, LlavaNextVideoForConditionalGeneration
-
-
 from tqdm import tqdm
-from sklearn.metrics import confusion_matrix
-from utils.data_utils import read_srt, srt_time_to_seconds
 from datetime import datetime
-from PIL import Image
 import torch
 import sys
 import warnings
 warnings.filterwarnings("ignore")
+
+from utils.data_utils import *
+from utils.video_utils import *
+
+def get_user_prompt(mode="baseline"):
+    if mode == "baseline":
+        user_prompt = ("You are a professional commentator for car racing games. You will be provided with few seconds"
+                       "interval video extracted from the whole game and your task is to either generate one sentence "
+                       "regarding the current state of the game or generate a <WAIT> if there is no developments in the game. You should: "
+                       "1)Identify the name of car diver through the legends provided and refer to cars by the name of the driver."
+                       "2) observe the game as a professional commentator would and decribe any developments."
+                       "3) Ignore the background information and refrain the describing the scenary. Just explain the game.")
+        conversation = [
+            {
+
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": user_prompt},
+                    {"type": "video"},
+                ],
+            },
+        ]
+        return conversation
+
+
 def get_utterence_timing(ground_truth,metadata):
     utterence_timing = [False] * int(metadata.get("duration"))
     utterences = []
@@ -26,63 +39,49 @@ def get_utterence_timing(ground_truth,metadata):
         utterences.append(gt.text)
     return utterences, utterence_timing
 
-def replace_video_with_images(text, frames):
-  return text.replace("<video>", "<image>" * frames)
+def baseline(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose = False):
 
-def get_video_info(path):
-    video = cv2.VideoCapture(path)
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = int(video.get(cv2.CAP_PROP_FPS))
-    duration = int(total_frames/fps)
-    return {"total_frames": total_frames, "frames_per_second": fps,
-            "duration":duration }
-def sample_frames(path, num_frames, start_frame = None, end_frame = None, format = "images"):
+    conversation = get_user_prompt("baseline")
+    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-    video = cv2.VideoCapture(path)
-    if start_frame is not None:
-        video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    ground_truth = read_srt(transcription_file)
+    video_metadata = get_video_info(mp4_file)
+    ref_utterences, ref_timing = get_utterence_timing(ground_truth, video_metadata)
+    num_frames_per_second = video_metadata["frames_per_second"]
 
-    if end_frame is None:
-        end_frame = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    pred_utterences = []
+    pred_timing = []
 
-    if start_frame - end_frame < num_frames:
-        print ()
-    interval = (end_frame - start_frame) // num_frames
-    frames = []
-    take_next_frame = False
+    for t in tqdm(range(video_metadata["duration"][::step]), total=video_metadata["duration"]/step):
 
+        video = sample_frames(mp4_file, num_frames_to_use, start_frame=t * num_frames_per_second,
+                              end_frame=(t + 1) * num_frames_per_second, format="video")
 
-    for i in range(end_frame - start_frame):
-        try:
-            ret, frame = video.read()
-            pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            if not ret:
-                continue
-            if i % interval == 0:
-                frames.append(pil_img)
-            if take_next_frame:
-                frames.append(pil_img)
-                take_next_frame = False
-        except Exception as e:
-            print (f"Failed to get frame number {i} for video: {path} due to Exception {e} \n Using the next frame instead.")
-            if i % interval == 0:
-                take_next_frame = True
+        inputs_video = processor(text=prompt, videos=video, padding=True, return_tensors="pt").to(model.device)
 
-    video.release()
-    if format == "video":
-        frames = [np.array(frame) for frame in frames]
-        frames = np.stack(frames, axis=0)
-    return frames
-def get_commentary_path(game_path):
-    game_path = os.path.basename(game_path)
-    commentary_path = [os.path.join(commentary_directory, file) for file in os.listdir(commentary_directory) if
-     file.endswith('.srt') and os.path.isfile(os.path.join(commentary_directory, file)) and "kyakkan" in file
-         and game_path in file]
-    if len(commentary_path) > 0:
-        commentary_path = commentary_path[0]
-    else:
-        commentary_path = None
-    return commentary_path
+        output = model.generate(**inputs_video, max_new_tokens=100, do_sample=False)
+        pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
+        pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
+        if "<WAIT>" in pred_utterence:
+            pred_timing.append(False)
+        else:
+            pred_timing.append(True)
+
+        pred_utterences.append(pred_utterence)
+        if t % 10 == 0 and verbose:
+            print(f"{t}: {pred_utterence}")
+
+    pred_utterences_cleaned = remove_repeatitions(pred_utterences)
+    out_file = write_logs(out_folder, pred_utterences_cleaned)
+
+    eval_metrics = compute_metrics(ref_timing, pred_timing)
+
+    if verbose:
+        print(eval_metrics)
+        print(f"Complete Commentary: {pred_utterences_cleaned}")
+
+    return pred_utterences
+
 
 if __name__ == '__main__':
     date_time = '{date:%Y-%m-%d_%H-%M-%S}'.format(date=datetime.now())
@@ -111,7 +110,7 @@ all_game_path = [os.path.join(video_directory,name) for name in os.listdir(video
 if n is None:
     n = len(all_game_path)
 for game_path in all_game_path[:n]:
-    transcription_file = get_commentary_path(game_path)
+    transcription_file = get_commentary_path(commentary_directory,game_path)
     if transcription_file is not None:
         mp4_file = [os.path.join(game_path,file) for file in os.listdir(game_path) if
                      file.endswith('.mp4') and os.path.isfile(os.path.join(game_path, file)) and "客観" in file][0]
@@ -119,24 +118,6 @@ for game_path in all_game_path[:n]:
         print (f"kyakkan commentary not available for game: {game_path}")
         continue
 
-
-#transcription_file = "transcriptions_whole_data_english/AC_150221-130155_R_ks_porsche_macan_mugello__kyakkan.merged.mp4_translated.srt"
-#transcription_file = os.path.join(folder, transcription_file)
-#mp4_file = "AC_150221-130155_R_ks_porsche_macan_mugello_/AC_150221-130155_R_ks_porsche_macan_mugello_客観.mp4"
-#mp4_file = os.path.join(video_directory, mp4_file)
-ground_truth = read_srt(transcription_file)
-video_metadata = get_video_info(mp4_file)
-ref_utterences, ref_timing = get_utterence_timing(ground_truth, video_metadata)
-num_frames_to_use = 3
-num_frames_per_second = video_metadata["frames_per_second"]
-pred_utterences = []
-pred_timing = []
-user_prompt = ("You are a professional commentator for car racing games. You will be provided with few seconds"
-                   "interval video extracted from the whole game and your task is to either generate one sentence "
-                   "regarding the current state of the game or generate a <WAIT> if there is no developments in the"
-                   "game. You should: 1)Identify the name of car diver through the legends provided and refer to cars by"
-               "the name of the driver. 2) observe the game as a professional commentator would and decribe any developments."
-               "3) Ignore the background information and refrain the describing the scenary. Just explain the game.")
 model_id = "llava-hf/LLaVA-NeXT-Video-7B-hf"
 
 
@@ -147,52 +128,12 @@ model = LlavaNextVideoForConditionalGeneration.from_pretrained(
     ).to(0)
 processor = LlavaNextVideoProcessor.from_pretrained(model_id)
 
-conversation = [
-        {
 
-            "role": "user",
-            "content": [
-                {"type": "text", "text": user_prompt},
-                {"type": "video"},
-            ],
-        },
-    ]
-prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-for t in tqdm(range(video_metadata["duration"]), total = video_metadata["duration"]):
+# Baseline without feedback loop
 
-    video = sample_frames(mp4_file, num_frames_to_use, start_frame=t*num_frames_per_second, end_frame=(t+1)*num_frames_per_second, format="video")
-
-    inputs_video = processor(text=prompt, videos=video, padding=True, return_tensors="pt").to(model.device)
-
-    output = model.generate(**inputs_video, max_new_tokens=100, do_sample=False)
-    pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
-    pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
-    if "<WAIT>" in pred_utterence:
-        pred_timing.append(False)
-    else:
-        pred_timing.append(True)
-
-    pred_utterences.append(pred_utterence)
-    if t % 10 == 0:
-        print(f"{t}: {pred_utterence}")
-
-complete_commentary = ""
-previous = ""
-for pred_utterence in pred_utterences:
-    if previous != pred_utterence:
-        complete_commentary += pred_utterence
-    previous = pred_utterence
-print (f"Complete Commentary: {complete_commentary}")
-out_file = os.path.join(out_folder, "logs.txt")
-print (f"Generation stored at {out_file}")
-with open(out_file, 'a') as the_file:
-    for t, ut in enumerate(pred_utterences):
-        the_file.write(f"{t}: {ut}\n")
-correlations = [1 if a == b else 0 for a, b in zip(ref_timing, pred_timing)]
-cm = confusion_matrix(ref_timing, pred_timing)
-print(correlations.count(1))
-print(cm)
-
+num_frames_to_use = 3
+step = 1
+basaeline_utterences = baseline(mp4_file, transcription_file, num_frames_to_use, step=step)
 
 
 
