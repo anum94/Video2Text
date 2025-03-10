67f73c48034a52944a7c41c829e18ce8fd86f6f7
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-10 16:39:58 +0900

    added feedback loop

--- main.py
+++ main.py
@@ -13,21 +13,37 @@ def get_user_prompt(mode="baseline"):
     if mode == "baseline":
         user_prompt = ("You are a professional commentator for car racing games. You will be provided with few seconds"
                        "interval video extracted from the whole game and your task is to either generate one sentence "
-                       "regarding the current state of the game or generate a <WAIT> if there is no developments in the game. You should: "
+                       "regarding the current state of the game or generate a <WAIT> if there is no developments in the game. "
+                       "You should focus on the following without being too verbose: "
                        "1)Identify the name of car diver through the legends provided and refer to cars by the name of the driver."
                        "2) observe the game as a professional commentator would and decribe any developments."
-                       "3) Ignore the background information and refrain the describing the scenary. Just explain the game.")
-        conversation = [
-            {
+                       "3) Ignore the background information and refrain the describing the scenery. Just explain the game.")
 
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": user_prompt},
-                    {"type": "video"},
-                ],
-            },
-        ]
-        return conversation
+    elif mode == "feedback_loop_init":
+        user_prompt = ("You are a professional commentator for car racing games. You will be provided with a video interval"
+                       "which represents the beginning of a race. Your task is to generate one to two sentences of commentary "
+                       "which describe the game in terms of number of players, their names and their cars. You should: "
+                       "1)Identify the name of car diver through the legends provided and refer to cars by the name of the driver."
+                       "2) Ignore the background information and refrain the describing the scenery. Just provide some brief"
+                       "initial information about the game without being too verbose. \nCommentary: ")
+
+    elif mode == "feedback_loop":
+        user_prompt = (
+            "You are a professional commentator for car racing games. You will be provided with a short video interval from a"
+            "complete race along with the partial commentary which summarizes the happening of the game before the provided"
+            "video interval. As you might know, sometimes commentators stay silent for a few seconds during the game."
+            "Your task is to first decide if the commentator should say something for the provided video interval"
+            "or stay quite. If you choose to stay quite then simply generate <WAIT>, otherwise generate one or two sentences "
+            "of commentary focus on the following without being too verbose:" 
+            "1)Identify the name of car diver through the legends provided and refer to cars by the name of the driver, "
+            "but don't say anything about it."
+            "2) Ignore the background information and refrain the describing the scenery. Just explain the game."
+            "3) observe the game as a professional commentator would and look for any developments while comparing to the "
+            "commentary from the previous intervals."
+            "If the state of the game as compared to the provided commentary has not changed, then generate <WAIT>, "
+            "otherwise generate a brief commentary. \n Previous Commentary: ")
+
+    return user_prompt
 
 
 def get_utterence_timing(ground_truth,metadata):
@@ -41,7 +57,17 @@ def get_utterence_timing(ground_truth,metadata):
 
 def baseline(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose = False):
 
-    conversation = get_user_prompt("baseline")
+    user_prompt = get_user_prompt("baseline")
+    conversation = [
+        {
+
+            "role": "user",
+            "content": [
+                {"type": "text", "text": user_prompt},
+                {"type": "video"},
+            ],
+        },
+    ]
     prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
     ground_truth = read_srt(transcription_file)
@@ -59,7 +85,7 @@ def baseline(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose 
 
         inputs_video = processor(text=prompt, videos=video, padding=True, return_tensors="pt").to(model.device)
 
-        output = model.generate(**inputs_video, max_new_tokens=100, do_sample=False)
+        output = model.generate(**inputs_video, max_new_tokens=max_new_tokens, do_sample=False)
         pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
         pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
         if "<WAIT>" in pred_utterence:
@@ -71,57 +97,77 @@ def baseline(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose 
         if t % 10 == 0 and verbose:
             print(f"{t}: {pred_utterence}")
 
-    pred_utterences_cleaned = remove_repeatitions(pred_utterences)
-    out_file = write_logs(out_folder, pred_utterences_cleaned)
+    #pred_utterences = remove_repeatitions(pred_utterences)
+    out_file = write_logs(out_folder, pred_utterences)
 
     eval_metrics = compute_metrics(ref_timing, pred_timing)
 
     if verbose:
         print(eval_metrics)
-        print(f"Complete Commentary: {pred_utterences_cleaned}")
+        print(f"Complete Commentary: {pred_utterences}")
 
     return pred_utterences
 
-def baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose = False):
-
-    conversation = get_user_prompt("baseline")
-    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+def baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step = 1, verbose = False,init_skip_frames=5):
 
     ground_truth = read_srt(transcription_file)
     video_metadata = get_video_info(mp4_file)
     ref_utterences, ref_timing = get_utterence_timing(ground_truth, video_metadata)
     num_frames_per_second = video_metadata["frames_per_second"]
 
-    pred_utterences = []
     pred_timing = []
-
+    pred_utterences = []
+    output_buffer_str = ""
+    t_buffer = 0
     for t in tqdm(range(0,video_metadata["duration"],step), total=video_metadata["duration"]/step):
 
-        video = sample_frames(mp4_file, num_frames_to_use, start_frame=t * num_frames_per_second,
+        video = sample_frames(mp4_file, num_frames_to_use, start_frame=(t-step+1-t_buffer) * num_frames_per_second,
                               end_frame=(t + 1) * num_frames_per_second, format="video")
 
-        inputs_video = processor(text=prompt, videos=video, padding=True, return_tensors="pt").to(model.device)
+        if t < init_skip_frames:
+            if t == 0:
+                user_prompt = get_user_prompt("feedback_loop_init")
+            else:
+                continue
+        else:
+            user_prompt = get_user_prompt("feedback_loop")
+            user_prompt += output_buffer_str
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": user_prompt},
+                    {"type": "video"},
+                ],
+            },
+        ]
+        prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-        output = model.generate(**inputs_video, max_new_tokens=100, do_sample=False)
+        inputs_video = processor(text=prompt, videos=video, padding=True, return_tensors="pt").to(model.device)
+        output = model.generate(**inputs_video, max_new_tokens=max_new_tokens, do_sample=False)
         pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
         pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
         if "<WAIT>" in pred_utterence:
             pred_timing.append(False)
+            t_buffer +=1
+
         else:
             pred_timing.append(True)
+            t_buffer = 0
 
         pred_utterences.append(pred_utterence)
+        output_buffer_str += pred_utterence
         if t % 10 == 0 and verbose:
             print(f"{t}: {pred_utterence}")
 
-    pred_utterences_cleaned = remove_repeatitions(pred_utterences)
-    out_file = write_logs(out_folder, pred_utterences_cleaned)
+    #pred_utterences = remove_repeatitions(pred_utterences)
+    out_file = write_logs(out_folder, pred_utterences)
     ref_timing = [ref for ref in range(0,len(ref_timing),step)]
     eval_metrics = compute_metrics(ref_timing, pred_timing)
 
     if verbose:
         print(eval_metrics)
-        print(f"Complete Commentary: {pred_utterences_cleaned}")
+        print(f"Complete Commentary: {pred_utterences}")
 
     return pred_utterences
 
@@ -181,9 +227,10 @@ processor = LlavaNextVideoProcessor.from_pretrained(model_id)
 # Baseline without feedback loop
 
 num_frames_to_use = 3
+max_new_tokens = 50
 if step is None:
-    step = 1
-baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step)
+    step = 2
+#baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step)
 baseline_feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step=step)
 
 
