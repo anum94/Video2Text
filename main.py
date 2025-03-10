a655277e169b163322a69b024f30441ab5b1c11f
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-10 18:39:02 +0900

    fixed code

--- main.py
+++ main.py
@@ -141,8 +141,8 @@ def baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step
     for t in tqdm(range(0,video_metadata["duration"],step), total=video_metadata["duration"]/step):
 
         print(f"Timestep: {t}")
-        print (output_buffer_str)
-        video = sample_frames(mp4_file, num_frames_to_use, start_frame=(t-step+1-t_buffer) * num_frames_per_second,
+        print (f"Output Buffer: {output_buffer_str}")
+        video = sample_frames(mp4_file, num_frames_to_use, start_frame=(t-step+1) * num_frames_per_second,
                               end_frame=(t + 1) * num_frames_per_second, format="video")
 
         if t < init_skip_frames:
