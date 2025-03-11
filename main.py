bf874d59a621448bf736169b5d55fabcdf6f4666
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-11 11:28:39 +0900

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore")
 from utils.data_utils import *
 from utils.video_utils import *
 
-def get_user_prompt(mode="baseline"):
+def get_user_prompt(mode="baseline", context="", step = 1):
     if mode == "baseline":
         user_prompt = ("You are a professional commentator for car racing games. You will be provided with few seconds"
                        "interval video extracted from the whole game and your task is to either generate one sentence "
@@ -29,12 +29,13 @@ def get_user_prompt(mode="baseline"):
 
     elif mode == "feedback_loop":
         user_prompt = (
-            "You are a professional commentator for car racing games. You will be provided with a short video interval from a"
-            "complete race along with the partial commentary which summarizes the happening of the game before the provided"
-            "video interval. As you might know, sometimes commentators stay silent for a few seconds during the game."
-            "Your task is to first decide if the commentator should say something for the provided video interval"
-            "or stay quite. If you choose to stay quite then simply generate <WAIT>, otherwise generate one or two sentences "
-            "of commentary focus on the following without being too verbose:" 
+            f"You are a professional commentator for car racing games and you are currently generating commentary for the following game: {context}"
+            f"You will be provided with a short video interval depicting the state of the game at a given interval of {step} seconds along with"
+            "some text that summarizes the game before the provided time interval."
+            "As you might know, sometimes commentators stay silent for a few seconds during the game."
+            "Your task is to first decide if you should say something for the provided time interval or choose to wait for some developments in the games"
+            "If you choose to stay quite then simply generate <WAIT>, otherwise generate one or two sentences "
+            "of commentary without being too verbose." 
             "1) Identify if the provided video has any new development as compared to the already provided commentary."
             "2) Ignore the background information and refrain the describing the scenery."
             "3) If the state of the game as compared to the provided commentary has not changed, then generate <WAIT>"
@@ -166,19 +167,20 @@ def baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step
         pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
         print(pred_utterence)
         pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
-        if t == 0:
-            output_buffer_str = pred_utterence
-        if pred_utterence.strip() == "<WAIT>" or pred_utterence.strip() == "<WAIT> The provided video interval shows no new developments compared to the already provided commentary. Therefore, I will stay quiet and not generate any commentary for this interval.":
+
+        if "<WAIT>" in pred_utterence:
             pred_timing.append(False)
         else:
             pred_timing.append(True)
-
-        if pred_utterence[:25].strip() == previous_generation[:25].strip():
-            pred_utterences.append("<WAIT>")
-        else:
             previous_generation = pred_utterence
-            pred_utterences.append(pred_utterence)
             output_buffer_str += pred_utterence
+            #if pred_utterence[:25].strip() == previous_generation[:25].strip():
+            #    pass
+        pred_utterences.append(pred_utterence)
+
+
+
+
 
         if t % 10 == 0 and verbose:
             print(f"{t}: {pred_utterence}")
