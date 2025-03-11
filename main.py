0f7b9f31452aa49f986d38cf81c3d95cf6710921
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-11 11:36:04 +0900

    .

--- main.py
+++ main.py
@@ -256,10 +256,8 @@ max_new_tokens = 50
 if step is None:
     step = 2
 
-try:
-    baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=10)
-except Exception as e:
-    print (f"Baseline method failed: {e}")
+
+baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=10)
 
 #baseline_feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, init_skip_frames=10, step=step, ICL=False)
 
