da984b564a0e56df291c0bc6f6aa5811f3a47308
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-13 11:32:54 +0800

    .

--- main.py
+++ main.py
@@ -274,9 +274,9 @@ def baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step
         inputs_video = processor(text=prompt, padding = True, videos=videos, return_tensors="pt").to(model.device)
         output = model.generate(**inputs_video, max_new_tokens=max_new_tokens, do_sample=do_sample, temperature = temp)
         pred_utterence = processor.decode(output[0][2:], skip_special_tokens=True)
-
-        pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
         print(pred_utterence)
+        pred_utterence = pred_utterence.split("ASSISTANT:")[-1]
+
 
         if "<WAIT>" in pred_utterence:
             pred_timing.append(False)
