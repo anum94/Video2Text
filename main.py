fc92bae1578d820636d9c9db9c212c6b90d508ee
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-17 17:02:38 +0900

    .

--- main.py
+++ main.py
@@ -374,9 +374,9 @@ if step is None:
     step = 1
 skip_frames = 20
 
-#baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step)
+baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step)
 
-#feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, init_skip_frames=skip_frames, step=step, ICL=False)
+feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, init_skip_frames=skip_frames, step=step, ICL=False)
 
 icl_feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use, init_skip_frames=skip_frames, step=step, ICL=icl_example_paths)
 
