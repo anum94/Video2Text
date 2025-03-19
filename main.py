3bc56bfdc1cc33b850ca021e514134bce4a4664d
Anum Afzal <anumafzaldogar@gmail.com>
2025-03-19 11:26:51 +0900

    .

--- main.py
+++ main.py
@@ -407,12 +407,12 @@ for i, game_path in enumerate(all_game_path[:n]):
 
     baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step, split_word = split_word)
 
-    #feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use,
-    #                                                  init_skip_frames=skip_frames, step=step, ICL=False, split_word = split_word)
+    feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use,
+                                                      init_skip_frames=skip_frames, step=step, ICL=False, split_word = split_word)
 
-    #icl_feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use,
-    #                                                      init_skip_frames=skip_frames, step=step,
-    #                                                      ICL=icl_example_paths, split_word = split_word)
+    icl_feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use,
+                                                          init_skip_frames=skip_frames, step=step,
+                                                          ICL=icl_example_paths, split_word = split_word)
 
 
 
