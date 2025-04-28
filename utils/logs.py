ab8b271688d1d794c7462d2e95928255a9969391
Anum Afzal <anumafzaldogar@gmail.com>
2025-04-28 14:45:29 +0200

    more evaluation metrics

--- utils/logs.py
+++ utils/logs.py
@@ -24,14 +24,18 @@ def write_to_wb(run_name, baseline_output:tuple, feedback_output:tuple, icl_outp
            mode=wandb_mode)
 
     b_eval_metrics = baseline_output[2]
+    b_generations = b_eval_metrics["pred_utterences"]
     b_pred_timing =b_eval_metrics["pred_timing"]
     ref_timing = b_eval_metrics["ref_timing"]
+    ref_generations = b_eval_metrics["ref_utterences"]
 
     f_eval_metrics = feedback_output[2]
     f_pred_timing = f_eval_metrics["pred_timing"]
+    f_generations = f_eval_metrics["pred_utterences"]
 
     i_eval_metrics = icl_output[2]
     i_pred_timing = i_eval_metrics["pred_timing"]
+    i_generations = i_eval_metrics["pred_utterences"]
 
 
     metrics_columns = (
@@ -50,7 +54,7 @@ def write_to_wb(run_name, baseline_output:tuple, feedback_output:tuple, icl_outp
     table = wandb.Table(columns=metrics_columns,data = [metrics_data] )
     wandb.log({"metrics_table": table}, commit=True)
 
-
+    # Plot Prediction Timing
     b_pred_timing = np.array(b_pred_timing).astype(int)
     f_pred_timing = np.array(f_pred_timing).astype(int)
     i_pred_timing = np.array(i_pred_timing).astype(int)
@@ -74,8 +78,6 @@ def write_to_wb(run_name, baseline_output:tuple, feedback_output:tuple, icl_outp
     axs[1,1].plot(x, i_pred_timing, marker='o', label='icl_pred_timing', linestyle='-', color='y')
     axs[1,1].legend()
 
-
-
     # Add labels and legend
     # plt.yticks([0, 1], ['False', 'True'])
     # plt.xticks(x)
@@ -85,5 +87,40 @@ def write_to_wb(run_name, baseline_output:tuple, feedback_output:tuple, icl_outp
 
     # Show grid
     plt.grid(axis='y', linestyle='--', alpha=0.7)
-    wandb.log({f"plot_{run_name}": wandb.Image(fig)})
+    wandb.log({f"plot_timing_{run_name}": wandb.Image(fig)})
+
+    # Plot Prediction Token Stats
+    b_gen_words = np.array([len(gen.split()) for gen in b_generations]).astype(int)
+    f_gen_words = np.array([len(gen.split()) for gen in f_generations]).astype(int)
+    i_gen_words = np.array([len(gen.split()) for gen in i_generations]).astype(int)
+    ref_gen_words = np.array([len(gen.split()) for gen in ref_generations]).astype(int)
+
+    # Define the indices for the x-axis
+    x = np.arange(len(b_gen_words))
+
+    # Plot the lists
+    fig, axs = plt.subplots(2, 2, figsize=(16, 12))
+    axs[0, 0].plot(x, b_gen_words, marker='o', label='baseline_gen_words', linestyle='-', color='b')
+    axs[0, 0].legend()
+
+    axs[0, 1].plot(x, ref_gen_words, marker='o', label='ref_gen_words', linestyle='-', color='g')
+    axs[0, 1].legend()
+
+    axs[1, 0].plot(x, f_gen_words, marker='o', label='feedback_gen_words', linestyle='-', color='r')
+    axs[1, 0].legend()
+
+    axs[1, 1].plot(x, i_gen_words, marker='o', label='icl_gen_words', linestyle='-', color='y')
+    axs[1, 1].legend()
+
+    # Add labels and legend
+    # plt.yticks([0, 1], ['False', 'True'])
+    # plt.xticks(x)
+
+    # plt.title(f"{run_name}")
+    plt.legend()
+
+    # Show grid
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    wandb.log({f"plot_worddist_{run_name}": wandb.Image(fig)})
+
     wandb.finish()
