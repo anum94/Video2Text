916f888e577f16ff0cd794df0afe218cc7c2d3de
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-06 14:40:37 +0200

    ..

--- utils/logs.py
+++ utils/logs.py
@@ -126,7 +126,8 @@ def write_to_wb(run_name, baseline_output:tuple, feedback_output:tuple, icl_outp
     wandb.finish()
     metrics = dict(zip(metrics_columns, metrics_data))
     additional_columns+= ["baseline_ref_timing", "baseline_pred_timing", "feedback_ref_timing",
-                          "feedback_pred_timing", "icl_ref_timing", "icl_pred_timing",]
+                          "feedback_pred_timing", "icl_ref_timing", "icl_pred_timing", "feedback_ROUGE_10%",
+                          "baseline_ROUGE_10%", "icl_ROUGE_10%"]
     for k,v in metrics["feedback_ROUGE_10%"].items():
         metrics[k] = v
     for k,v in metrics["baseline_ROUGE_10%"].items():
