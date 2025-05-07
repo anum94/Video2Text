3734f39e8d7dd7f84807de15d8414d0f2080ac7f
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-07 21:41:59 +0200

    .

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ module load cuda/12.6/12.6.1
 module list
 source $HOME/anum/Video2Text/py3.9/bin/activate
 cd $HOME/anum/Video2Text/
-python main.py --dir "/groups/gac50547/RaceCommentary" --n 50 --icl True --step 20 --k 4 --frames 1 --context_window 4096
+python main.py --dir "/groups/gac50547/RaceCommentary" --n 1 --icl True --step 20 --k 4 --frames 1 --context_window 4096 --wb False
