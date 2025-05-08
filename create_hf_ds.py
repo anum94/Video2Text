7a59b11f4bb7f333a392201ab5e343f6e5720fe0
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-08 14:47:35 +0200

    pushing files to create train / test and run main.py using text split

--- create_hf_ds.py
+++ create_hf_ds.py
@@ -2,6 +2,45 @@ import argparse, os
 from main import get_commentary_path
 from pandas import DataFrame
 from datasets import Dataset
+
+def create_ds(folder):
+    video_directory = "recordings"
+    video_directory = os.path.join(folder, video_directory)
+
+    commentary_directory = "transcriptions_whole_data_english"
+    commentary_directory = os.path.join(folder, commentary_directory)
+    hf_dataset = []
+
+    all_game_path = [os.path.join(video_directory, name) for name in os.listdir(video_directory) if
+                     os.path.isdir(os.path.join(video_directory, name))]
+
+    count = 0
+    for i, game_path in enumerate(all_game_path):
+        transcription_file = get_commentary_path(commentary_directory, game_path)
+        if transcription_file is not None:
+            mp4_file = [os.path.join(game_path, file) for file in os.listdir(game_path) if
+                        file.endswith('.mp4') and os.path.isfile(os.path.join(game_path, file)) and "客観" in file][0]
+        else:
+            # print (f"kyakkan commentary not available for game: {game_path}")
+            count += 1
+            continue
+
+        sample_name = os.path.dirname(mp4_file).split('/')[-1]
+        dataset_item = {"sample_name": sample_name,
+                        "video_path": mp4_file,
+                        "srt_path": transcription_file, }
+        hf_dataset.append(dataset_item)
+
+    hf_dataset = Dataset.from_list(hf_dataset)
+    dataset_processed = hf_dataset.shuffle(seed=42)
+    print(f"kyakkan commentary not available for {count} samples.")
+    print(dataset_processed)
+    hf_dataset = dataset_processed.train_test_split(test_size=400)
+    dir = "RaceCommentaryEn/"
+    os.makedirs(dir, exist_ok=True)
+    hf_dataset.save_to_disk(dir)
+    return dir
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
@@ -13,41 +52,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     folder = args.dir
-    video_directory = "recordings"
-    video_directory = os.path.join(folder,video_directory)
+    create_ds(folder=folder)
 
-    commentary_directory = "transcriptions_whole_data_english"
-    commentary_directory = os.path.join(folder,commentary_directory)
-    hf_dataset = []
-
-    all_game_path = [os.path.join(video_directory, name) for name in os.listdir(video_directory) if
-                     os.path.isdir(os.path.join(video_directory, name))]
-
-    count = 0
-    for i, game_path in enumerate(all_game_path):
-        transcription_file = get_commentary_path(commentary_directory,game_path)
-        if transcription_file is not None:
-            mp4_file = [os.path.join(game_path,file) for file in os.listdir(game_path) if
-                         file.endswith('.mp4') and os.path.isfile(os.path.join(game_path, file)) and "客観" in file][0]
-        else:
-            #print (f"kyakkan commentary not available for game: {game_path}")
-            count += 1
-            continue
-
-        sample_name = os.path.dirname(mp4_file).split('/')[-1]
-        dataset_item = {"sample_name": sample_name,
-                        "video": mp4_file,
-                        "srt": transcription_file, }
-        hf_dataset.append(dataset_item)
-
-    hf_dataset = Dataset.from_list(hf_dataset)
-    dataset_processed = hf_dataset.shuffle(seed=42)
-    print (f"kyakkan commentary not available for {count} samples.")
-    print (dataset_processed)
-    hf_dataset = dataset_processed.train_test_split(test_size=400)
-    dir = "RaceCommentaryEn/"
-    os.makedirs(dir, exist_ok=True)
-    hf_dataset.save_to_disk(dir)
     #train_dataset, test_dataset = hf_dataset['train'].with_format("torch"), hf_dataset['test'].with_format("torch")
 
 
