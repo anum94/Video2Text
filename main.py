cd971008576c9073765093bff130a99847c6ed74
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-10 23:31:06 +0200

    ..

--- main.py
+++ main.py
@@ -352,8 +352,8 @@ def realtime_feedback_loop(mp4_file, transcription_file, num_frames_to_use, step
 
     # 書き出しと評価
     mode = "realtime_icl_feedback_loop" if ICL else "realtime_feedback_loop"
-    ref_timing_s = ref_timing[pred_utterences_step]
-    ref_utterences_s = ref_utterences[pred_utterences_step]
+    ref_timing_s = [ref_timing[i] for i in pred_utterences_step]
+    ref_utterences_s = [ref_utterences[i]  for i in pred_utterences_step]
 
     pred_srt_file = write_logs(out_folder, pred_utterences, pred_utterences_step, mode=mode,
                                talking_speed_sample=icl_transcription_file)
