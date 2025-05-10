87b7147692b4ca266d524ef9bdfeb7064e866950
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-10 23:07:59 +0200

    ..

--- finetune.sh
+++ finetune.sh
@@ -3,5 +3,5 @@ module load cuda/12.6/12.6.1
 module list
 source $HOME/anum/Video2Text/py3.9/bin/activate
 cd $HOME/anum/Video2Text/
-python finetune.py --dir "/groups/gac50547/RaceCommentary/" --frames 2 --step 2 --n 2000 --hf_dataset "RaceCommentaryEn"
+python finetune.py --dir "/groups/gac50547/RaceCommentary/" --frames 2 --step 2 --n_train 50000 --hf_dataset "RaceCommentaryEn" --use_existing True --context_window 2048 --n_test 50
 
