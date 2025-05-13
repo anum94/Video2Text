cb1dccc7b7d4c70303d0c140867d9b28098f1b68
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-13 21:49:45 +0200

    Debugging for qwen

--- main.py
+++ main.py
@@ -671,7 +671,7 @@ if __name__ == '__main__':
         if "qwen" in model_name:
             model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
                 model_id, torch_dtype="auto", device_map="auto", load_in_8bit=True, low_cpu_mem_usage=True
-            )
+            ).to(0)
             processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
 
 
