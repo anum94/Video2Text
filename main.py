79bfa11911afce00f4e55961caa7fcec38d5e68c
Anum Afzal <40362161+anum94@users.noreply.github.com>
2025-05-14 15:01:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -784,9 +784,6 @@ if __name__ == '__main__':
                                                                   context_window=context_window, logs_dir=out_folder,
                                                                   model_name=model_name)
 
-            icl_feedback_loop_generation = baseline_generation
-            feedback_loop_generation = baseline_generation
-            realtime_loop_generation = baseline_generation
             run_name = f"{sample_name}_step_{step}_k_{k}_frames_{num_frames_to_use}"
             config = {"model": model_id, "step": step, "# frame": num_frames_to_use, "sample_name": sample_name, "k": k,
                       "dataset": hf_dataset_path
