7a84d1aead5daacadcacf610a0d4549a8683ab47
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-15 09:40:40 +0200

    updated prompts for sambra dataset

--- main.py
+++ main.py
@@ -24,15 +24,16 @@ from datasets import Dataset, load_dataset, concatenate_datasets
 import datasets
 import re
 
-def get_user_prompt(mode="baseline", context="", step = 1, force=False):
-    #todo: move prompts to a yaml file
+
+def get_user_prompt(mode="baseline", context="", step=1, force=False):
+    # todo: move prompts to a yaml file
     if mode == "baseline":
         user_prompt = ("You are a professional commentator for car racing games. You are provided with a video clip"
                        "from an ongoing car racing game and commentary generated for the game so far. \n"
                        "Your task is to generate 1 - 2 line of commentary to describe it. Ignore the background information and refrain the describing the scenery too much."
                        "\nDescribe this scene as a single-sentence commentary for making audience immersed.  If you have nothing to say, generate a <WAIT> token."
                        )
-        
+
     elif mode == "baseline_ja":
         user_prompt = ("あなたはカーレースのプロの実況者です。以下に示すのは現在進行中のレースのビデオクリップと、これまでに生成された実況です。\n"
                        "このシーンを1文で説明する日本語の実況を生成してください。\n"
@@ -49,7 +50,7 @@ def get_user_prompt(mode="baseline", context="", step = 1, force=False):
                        "2) Ignore the background information and refrain the describing the scenery."
                        "3) Initial information about the game without being too verbose."
                        )
-        
+
     elif mode == "feedback_loop_init_ja":
         user_prompt = ("あなたはカーレースのプロの実況者です。これからレース開始時のビデオクリップが提示されます。\n"
                        "それに対して1文の日本語実況を生成してください。\n"
@@ -63,23 +64,23 @@ def get_user_prompt(mode="baseline", context="", step = 1, force=False):
     elif mode == "feedback_loop":
         if force:
             user_prompt = ("You are a professional commentator for car racing games. You are provided with a video clip"
-                "from an ongoing car racing game and commentary generated for the game so far."
-                 f"\nPrevious generated Commentary: \n{context}\n"
-                 "Your task is to compare the given video with the previously generated commentary. \n"
-                "1) Identify if the video has any new development as compared to the already provided commentary. \n"
-                "2) Ignore the background information and refrain the describing the scenery too much. \n"
-                "3) If there are new developments in the provided video, then generate 1 - 2 line of commentary to describe it. \n"
-            )
+                           "from an ongoing car racing game and commentary generated for the game so far."
+                           f"\nPrevious generated Commentary: \n{context}\n"
+                           "Your task is to compare the given video with the previously generated commentary. \n"
+                           "1) Identify if the video has any new development as compared to the already provided commentary. \n"
+                           "2) Ignore the background information and refrain the describing the scenery too much. \n"
+                           "3) If there are new developments in the provided video, then generate 1 - 2 line of commentary to describe it. \n"
+                           )
         else:
             user_prompt = ("You are a professional commentator for car racing games. You are provided with a video clip"
-                "from an ongoing car racing game and commentary generated for the game so far."
-                 f"\nPrevious generated Commentary: \n{context}\n"
-                 "Your task is to compare the given video with the previously generated commentary. \n "
-                "1) Identify if the video has any new development as compared to the already provided commentary. \n"
-                "2) Ignore the background information and refrain the describing the scenery too much.\n"
-                "3) If the state of the game as compared to the provided commentary has not changed, then generate <WAIT>\n"
-                "4) If there are new developments in the provided video, then generate 1 - 2 line of commentary to describe it.\n"
-            )
+                           "from an ongoing car racing game and commentary generated for the game so far."
+                           f"\nPrevious generated Commentary: \n{context}\n"
+                           "Your task is to compare the given video with the previously generated commentary. \n "
+                           "1) Identify if the video has any new development as compared to the already provided commentary. \n"
+                           "2) Ignore the background information and refrain the describing the scenery too much.\n"
+                           "3) If the state of the game as compared to the provided commentary has not changed, then generate <WAIT>\n"
+                           "4) If there are new developments in the provided video, then generate 1 - 2 line of commentary to describe it.\n"
+                           )
     elif mode == "feedback_loop_ja":
         if force:
             user_prompt = ("あなたはカーレースのプロの実況者です。以下に示すのは現在進行中のレースのビデオクリップと、これまでに生成された実況です。\n"
@@ -102,18 +103,16 @@ def get_user_prompt(mode="baseline", context="", step = 1, force=False):
         if force:
             user_prompt = ("あなたは大乱闘スマッシュブラザーズのプロの実況者です。以下に示すのは現在進行中のレースのビデオクリップと、これまでに生成された実況です。\n"
                            f"\nこれまでの実況:\n{context}\n"
-                            "このシーンを1文で説明する日本語の実況を生成し視聴者を楽しませてください。\n"
-                            "観客が没入できるよう驚きや感嘆句も含めてエキサイティングな実況となるよう心がけてください。話すべきことがなければ <WAIT> を出力してください。")
+                           "このシーンを1文で説明する日本語の実況を生成し視聴者を楽しませてください。\n"
+                           "観客が没入できるよう驚きや感嘆句も含めてエキサイティングな実況となるよう心がけてください。話すべきことがなければ <WAIT> を出力してください。")
 
         else:
             user_prompt = ("あなたは大乱闘スマッシュブラザーズのプロの実況者です。以下に示すのは現在進行中のレースのビデオクリップと、これまでに生成された実況です。\n"
                            f"\nこれまでの実況:\n{context}\n"
-                            "このシーンを1文で説明する日本語の実況を生成し視聴者を楽しませてください。\n"
-                            "観客が没入できるよう驚きや感嘆句も含めてエキサイティングな実況となるよう心がけてください。話すべきことがなければ <WAIT> を出力してください。")
-
+                           "このシーンを1文で説明する日本語の実況を生成し視聴者を楽しませてください。\n"
+                           "観客が没入できるよう驚きや感嘆句も含めてエキサイティングな実況となるよう心がけてください。話すべきことがなければ <WAIT> を出力してください。")
 
     return user_prompt
-
 def create_ds(folder):
     video_directory = "recordings"
     video_directory = os.path.join(folder, video_directory)
@@ -268,7 +267,7 @@ def run_inference(model_name, model, processor, prompt, videos, ICL=False, conte
 def identify_dataset(transcription_file):
     if "transcriptions_whole_data_english" in transcription_file:
         return "" # race game in English
-    elif "transcriptions_smabra" in transcription_file:
+    elif "smabra_ja" in transcription_file:
         return "_smabra" # smash corpus
     else:
         return "_ja" # race game in Japanese
@@ -755,7 +754,7 @@ if __name__ == '__main__':
 
 
         else:
-            model = LlavaNextVideoForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True,).to(0)
+            model = LlavaNextVideoForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True,load_in_4bit=True,).to(0)
             processor = LlavaNextVideoProcessor.from_pretrained(model_id, use_fast = True)
     else:
         model = None
@@ -806,6 +805,7 @@ if __name__ == '__main__':
                                                                   k = 4 , processor=processor, model=model,
                                                                   context_window=context_window, logs_dir=out_folder,
                                                                   model_name=model_name)
+
 
             run_name = f"{sample_name}_step_{step}_k_{k}_frames_{num_frames_to_use}"
             config = {"model": model_id, "step": step, "# frame": num_frames_to_use, "sample_name": sample_name, "k": k,
