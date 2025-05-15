d77bea4ed5b8abff89344436696e38bbfca1d4bc
Anum Afzal <anumafzaldogar@gmail.com>
2025-05-15 12:50:50 +0200

    debug mode

--- prepare_evaluation_dataset.py
+++ prepare_evaluation_dataset.py
@@ -74,10 +74,7 @@ if __name__ == '__main__':
     evaluation_metrics = ["KEI", "WAIT-NESS", "Naturalness", "Logical_Coherence"]
     print(len(logs_list))
     df = pd.DataFrame(logs_list)#.dropna()
-    for col in df:
-        print (col)
-        print(df[col].unique())
-    df = df[((df['step'] == 2) & (df['frames_used'] == 1)) & (df['k'] == 2) ]
+    df = df[((df['step'] == '2') & (df['frames_used'] == '1')) & (df['k'] == '8') ]
 
     print (len(df))
     df_samples = df.groupby('sample')
