d99604d9a6f04d04974a31ea13db8a19306bdf2a
Anum Afzal <anumafzaldogar@gmail.com>
2025-10-12 20:38:58 +0200

    ..

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ module load cuda/12.6/12.6.1
 module list
 source $HOME/anum/Video2Text/py3.9/bin/activate
 cd $HOME/anum/Video2Text/
-python main.py --n 100 --icl True --model_name "llava7b" --step 2 --k 8 --frames 1 --context_window 8192 --wb True --hf_dataset "/home/aac12020fu/ishigaki/Video2Text/SmabraDataJa"
+python main.py --n 100 --icl True --model_name "qwen7b" --step 2 --k 8 --frames 1 --context_window 8192 --wb True --hf_dataset "/home/aac12020fu/ishigaki/Video2Text/SmabraDataJa"
