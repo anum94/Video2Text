2ffaad54e0a4357c1991079b4ec1e1dd35548038
Anum Afzal <anumafzaldogar@gmail.com>
2025-10-12 19:21:17 +0200

    ..

--- main.py
+++ main.py
@@ -800,7 +800,7 @@ if __name__ == '__main__':
 
 
             print ("Baseline")
-            baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step, split_word = split_word)
+            #baseline_generation = baseline(mp4_file, transcription_file, num_frames_to_use, step=step, split_word = split_word)
 
             print ("Feedback")
             feedback_loop_generation = baseline_feedback_loop(mp4_file, transcription_file, num_frames_to_use,
